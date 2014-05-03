f55ff8890f0303b7fc1895cd52a632c8ab96e50c
Matus Tejiscak <ziman@functor.sk>
2014-05-03 15:51:42 +0100

    Use various input sizes.

--- benchdata.py
+++ benchdata.py
@@ -1,10 +1,17 @@
 N_runs = 5
+TIMEOUT_sec = 15
 
 BENCHMARKS = [
     ('001-shl-bin', {
+        'input_sizes': [1, 10, 100, 1000, 10000, 100000],
+        'mkinput': lambda x: "%s\n" % x,
     }),
     ('002-add-bins', {
+        'input_sizes': [],
+        'mkinput': None,
     }),
     ('003-palindrome', {
+        'input_sizes': [],
+        'mkinput': None,
     }),
 ]
--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 
 import re
 import os
+import sys
 import subprocess
 import threading
 import datetime
@@ -17,59 +18,79 @@ def delete(fnames):
         except OSError:
             pass  # file does not exist, ignore
 
-def exec_timeout(command, timeout_sec):
+def exec_timeout(command, timeout_sec, stdin=None):
     """ Return the process's runtime in seconds or None if timed out. """
 
-    sub = subprocess.Popen(command)
-    thread = threading.Thread(target=sub.communicate)
-    thread.start()
+    with open('/dev/null', 'wb') as dev_null:
+        sub = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=dev_null, stderr=dev_null)
+        thread = threading.Thread(target=lambda: sub.communicate(input=stdin))
+        thread.start()
 
-    t1 = datetime.datetime.now()
-    thread.join(timeout_sec)
-    t2 = datetime.datetime.now()
+        t1 = datetime.datetime.now()
+        thread.join(timeout_sec)
+        t2 = datetime.datetime.now()
 
-    if thread.is_alive():
-        sub.terminate()
-        thread.join()
-        return None # timed out
-    else:
-        return (t2-t1).total_seconds()
+        if thread.is_alive():
+            sub.terminate()
+            thread.join()
+            return None # timed out
+        else:
+            return (t2-t1).total_seconds()
 
-def exec_multi(attempts, command, timeout_sec, cleanup=None):
+def prn(s):
+    sys.stdout.write(s)
+    sys.stdout.flush()
+
+def exec_multi(attempts, command, timeout_sec, cleanup=None, stdin=None):
     ts = []
-    for _ in xrange(attempts):
+    for i in xrange(attempts):
         if cleanup:
             cleanup()
 
-        t = exec_timeout(command, timeout_sec)
+        t = exec_timeout(command, timeout_sec, stdin=stdin)
         if t is not None:
             ts.append(t)
 
+        prn('%d ' % (i+1))
+
     N = len(ts)
-    mean = sum(ts) / N
+    mean = sum(ts) / N if N else None
     ssq = sum((t - mean)**2 for t in ts)
 
     return {
         'mean': mean,
-        'stdev': math.sqrt(ssq / (N-1)),
+        'stdev': math.sqrt(ssq / (N-1)) if N != 1 else None,
         'N': N,
         'attempts': attempts,  # including timeouts
     }
 
 def run_benchmark(bname, binfo):
+    print 'Running benchmark: %s' % bname
+
     def cleanup():
         delete(
             fn for fn in os.listdir('.')
             if fn.endswith('.ibc') or fn == bname
         )
 
+    prn('  compiling: ')
     t_comp = exec_multi(N_runs,
         ['idris', '--warnreach', bname + '.idr', '-o', bname],
-        timeout_sec=30,
+        timeout_sec=TIMEOUT_sec,
         cleanup=cleanup,
     )
+    print ''
 
     t_runtime = []
+    for isize in binfo['input_sizes']:
+        prn('  input size %d: ' % isize)
+        t_run = exec_multi(N_runs,
+            ['./' + bname],
+            timeout_sec=TIMEOUT_sec,
+            stdin=binfo['mkinput'](isize),
+        )
+        t_runtime.append((isize, t_run))
+        print ''
 
     cleanup()
 
