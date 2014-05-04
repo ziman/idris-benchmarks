00c799b070c1ab23ed5f8c1c78471b193e613dba
Matus Tejiscak <ziman@functor.sk>
2014-05-04 12:17:47 +0100

    Improve graphs.

--- benchdata.py
+++ benchdata.py
@@ -18,15 +18,18 @@ BENCHMARKS = [
         'input_sizes': linspace(1, 26, GRAN),
         'mk_input': lambda x: "%s\n" % x,
         'mk_output': lambda x: '0' * x + '1',
+        'units': 'left-shift amount, in bits',
     }),
     ('002-add-bins', {
         'input_sizes': linspace(1, 200, GRAN),
         'mk_input': lambda w: "%d\n%s\n%s\n%d\n" % (w, '1' + '0'*(w-1), '1' + '0'*(w-1), 100*1000),
         'mk_output': lambda w: '1' + '0'*w,
+        'units': 'word width, in bits',
     }),
     ('003-palindrome', {
         'input_sizes': linspace(1, 2*1024, GRAN),
         'mk_input': lambda k: 'a' + 'b'*k + 'a',
         'mk_output': lambda k: 'yes',
+        'units': 'input length, in characters',
     }),
 ]
--- compare.py
+++ compare.py
@@ -5,6 +5,8 @@ import argparse
 import jinja2
 import subprocess
 
+from benchdata import *
+
 def rootname(inp):
     return '.'.join(inp.split('/')[-1].split('.')[:-1])
 
@@ -16,6 +18,7 @@ def main(args):
     inputs = [load_input(inp) for inp in args.inputs]
     inames = [rootname(inp) for inp in args.inputs]
     bnames = [n for n, _ in inputs[0]]
+    units  = {bname: info['units'] for bname, info in BENCHMARKS} 
 
     compilation = {}
     runtime = {}
@@ -31,50 +34,82 @@ def main(args):
 
     for bname in bnames:
         # data for R
-        with open('tmp/%s-runtime.txt' % bname, 'w') as f:
-            f.write('"version" "dsize" "runtime"\n')
+        try:
+            with open('tmp/%s-runtime-R.txt' % bname, 'w') as f:
+                f.write('"version" "dsize" "runtime"\n')
 
-            for iname in inames:
-                for isize, stat in runtime[bname][iname]:
-                    for t in stat['ts']:
-                        f.write('"%s" %f %f\n' % (iname, isize, t))
+                for iname in inames:
+                    for isize, stat in runtime[bname][iname]:
+                        for t in stat['ts']:
+                            f.write('"%s" %f %f\n' % (iname, isize, t))
 
-        with open('tmp/%s-compilation.txt' % bname, 'w') as f:
-            f.write('"version" "time"\n')
+            with open('tmp/%s-compilation-R.txt' % bname, 'w') as f:
+                f.write('"version" "time"\n')
 
-            for iname in inames:
-                for t in compilation[bname][iname]['ts']:
-                    f.write('"%s" %f\n' % (iname, t))
+                for iname in inames:
+                    for t in compilation[bname][iname]['ts']:
+                        f.write('"%s" %f\n' % (iname, t))
+        except KeyError:
+            pass # XXX: ts not found
 
         # data for Gnuplot
-        plots = []
+        cplots = []
+        rplots = []
 
         for i, iname in enumerate(inames):
-            with open('tmp/%s-%s-compilation.txt' % (bname, iname), 'w') as f:
+            with open('tmp/%s-%s-compilation-gpl.txt' % (bname, iname), 'w') as comp_gpl:
                 c = compilation[bname][iname]
-                f.write('%g %g\n' % (c['mean'], c['stdev']))
+                comp_gpl.write('%d %g %g\n' % (i, c['mean'], c['stdev']))
 
-            with open('tmp/%s-%s-runtime.txt' % (bname, iname), 'w') as f:
+            cplots.append('"tmp/%s-%s-compilation-gpl.txt" with boxerrorbars title "%s" fill pattern 10 lw 2'
+                % (bname, iname, iname))
+
+            with open('tmp/%s-%s-runtime-gpl.txt' % (bname, iname), 'w') as f:
                 for isize, stat in runtime[bname][iname]:
                     f.write('%g %g %g\n' % (isize, stat['mean'], stat['stdev']))
 
-            plots.append('"tmp/%s-%s-runtime.txt" with errorbars title "" lw 1' % (bname, iname))
-            plots.append('"tmp/%s-%s-runtime.txt" using 1:2 with lines lw 1 title "%s"' % (bname, iname, iname))
+            rplots.append('"tmp/%s-%s-runtime-gpl.txt" with errorbars title "" lw 1' % (bname, iname))
+            rplots.append('"tmp/%s-%s-runtime-gpl.txt" using 1:2 with lines lw 1 title "%s"' % (bname, iname, iname))
+
+        with open('tmp/%s-compilation.gpl' % bname, 'w') as f:
+            f.write("""
+                set title '{0}';
+                set ylabel 'Compilation time (seconds)';
+                set xlabel 'Version';
+                set term pngcairo size 640,480 fontscale 0.75 background '#ffffff';
+                set output '{1}';
+                set xtics nomirror ({2});
+                set xrange [-1:{3}];
+                set yrange [0:{4}];
+                set grid;
+                set boxwidth 0.75;
+                plot {5};
+            """.format(
+                bname,
+                'report/%s-compilation.png' % bname,
+                ', '.join('"%s" %d' % (iname, i) for i, iname in enumerate(inames)),
+                len(inames),
+                1.2 * max(c['mean'] + c['stdev'] for c in (d for d in compilation[bname].itervalues())),
+                ', '.join(cplots),
+            ) + '\n')
 
         with open('tmp/%s-runtime.gpl' % bname, 'w') as f:
             f.write("""
                 set title '{0}';
                 set ylabel 'Runtime (seconds)';
-                set xlabel 'Data size';
+                set xlabel 'Data size ({1})';
                 set term pngcairo size 640,480 fontscale 0.75 background '#ffffff';
-                set output '{1}';
-                plot \\\n  {2};
+                set output '{2}';
+                set grid;
+                plot \\\n  {3};
             """.format(
                 bname,
+                units[bname],
                 'report/%s-runtime.png' % bname,
-                ',\\\n  '.join(plots),
+                ',\\\n  '.join(cplots),
             ) + '\n')
 
+        subprocess.check_call(['gnuplot', 'tmp/%s-compilation.gpl' % bname])
         subprocess.check_call(['gnuplot', 'tmp/%s-runtime.gpl' % bname])
 
 parser = argparse.ArgumentParser(description='Generate an HTML report from benchmark JSONs.')
